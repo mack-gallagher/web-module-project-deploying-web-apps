146b1385632f23f385656369ce345a8b2ef9d83c
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-27 05:26:53 -0500

    modify clarifying headers for formatting

--- mack-web-app/src/App.js
+++ mack-web-app/src/App.js
@@ -11,10 +11,10 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h2>Hardcore Chess</h2>
+        <h1>Hardcore Chess</h1>
         <h4>Click any piece once to select it for moving.</h4>
         <h4>
-          Click anywhere on the board to move your current active piece, making a capture if possible
+          Click anywhere on the board to move your current active piece, making a capture if possible.
         </h4>
         <h4>Be warned: the board will let you make any move, legal or no!</h4>
         <Board />
